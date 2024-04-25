cf7dd22fa91ac7eea9310f5fae8acef8c558ddc2
Speiger <speiger@gmx.net>
2024-04-25 17:10:22 +0200

    Fixed crash with Undocumented types

--- src/main/java/carbonconfiglib/gui/impl/forge/ForgeLeaf.java
+++ src/main/java/carbonconfiglib/gui/impl/forge/ForgeLeaf.java
@@ -73,6 +73,10 @@ public class ForgeLeaf implements IConfigNode
 		}
 	}
 	
+	public boolean isValid() {
+		return type != null;
+	}
+	
 	@Override
 	public List<IConfigNode> getChildren() { return null; }
 	
